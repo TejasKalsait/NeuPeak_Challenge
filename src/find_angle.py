de23542193f5e5c15dd292b491cc58493772d6a9
TejasKalsait <kalsaittejas10@gmail.com>
2023-11-22 02:28:31 -0500

    Added Function to calculate x&y interecepts
    Added a function that takes inputs edge wall points and returns
    the x and y intercepts for both walls
    using this I plan to calculate angle

--- src/find_angle.py
+++ src/find_angle.py
@@ -25,15 +25,15 @@ class Angle_Publisher(Node):
         sorted_indices = np.argsort(row_pointcloud[:, 1])[::-1]
         row_pointcloud = row_pointcloud[sorted_indices]
 
-        print("After deleting height-axis, removing duplicates, and sorting with x-values, (x, y) is", row_pointcloud[0, 0], row_pointcloud[0, 1], "and shape is", row_pointcloud.shape)
+        print("After deleting height-axis, removing duplicates, and sorting with x-values, shape is", row_pointcloud.shape)
 
         return row_pointcloud
     
-    def extractEdgePoints(self, row_pointcloud, plot):
+    def extractEdgePoints(self, row_pointcloud, plot = False):
 
         left_points, right_points = [], []
         max_on_y = row_pointcloud[0,1]
-        num_of_pivots = 30
+        num_of_pivots = 4
         partitions = np.linspace(max_on_y, 0.0, num_of_pivots)
 
         print("Extracting Edge points")
@@ -81,13 +81,45 @@ class Angle_Publisher(Node):
             plt.show()
 
         return left_points, right_points
+    
+    def calculateInterceptfromPoints(self, left_points, right_points):
+
+        # Left line
+        lx1, ly1 = left_points[0]
+        lx2, ly2 = left_points[1]
+
+        try:
+            l_slope = (ly2 - ly1) / (lx2 - lx1)
+        except:
+            print("Found the left wall fully straight")
+            return None, None, None, None
+        
+        # Right line
+        rx1, ry1 = right_points[0]
+        rx2, ry2 = right_points[1]
+
+        try:
+            r_slope = (ry2 - ry1) / (rx2 - rx1)
+        except:
+            print("Found the right wall fully straight")
+            return None, None, None, None
+        
+        print("Left wall slope is", l_slope, "and right wall slope is", r_slope)
+
+        l_y_intercept = -(l_slope * lx2) + ly1
+        l_x_intercept = lx1 - (ly1 / l_slope)
+
+        r_y_intercept = -(r_slope * rx2) + ry1
+        r_x_intercept = rx1 - (ry1 / r_slope)
+
+        return (l_x_intercept, l_y_intercept, r_x_intercept, r_y_intercept)
 
     
     def anglePublishOnce(self):
 
         try:
             # Opening file
-            row_np_array = np.load("../Depth_Output/2.npz")
+            row_np_array = np.load("../Depth_Output/1.npz")
             row_pointcloud = row_np_array['arr_0']
         except Exception as e:
             print(f"Error opening the .npz file {e}")
@@ -96,7 +128,20 @@ class Angle_Publisher(Node):
 
         left_edge_points, right_edge_points = self.extractEdgePoints(row_pointcloud, plot = True)
 
+        left_x_intercept, left_y_intercept, right_x_intercept, right_y_intercept = self.calculateInterceptfromPoints(left_edge_points, right_edge_points)
+
+        if not left_x_intercept or not right_x_intercept:
+            #Publish 0 deg
+            print("Zero Degree Turn")
+
+        # Left line angle
+        left_angle_turn = 90 + np.degrees(np.arctan(left_y_intercept/left_x_intercept))
+
+        # Right line angle
+        right_angle_turn = 90 + np.degrees(np.arctan(right_y_intercept/right_x_intercept))
+        print((left_angle_turn + right_angle_turn) / 2.0)
         
+
 
 
 def main(args=None):
