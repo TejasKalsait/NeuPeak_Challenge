d79c81a54d0d8618a62bf79c8a845d7e33f1b2c4
TejasKalsait <kalsaittejas10@gmail.com>
2023-11-24 00:11:18 -0500

    Added print statements in test_row_nav.py

--- src/test_row_nav.py
+++ src/test_row_nav.py
@@ -9,7 +9,9 @@ class TestMyMath(unittest.TestCase):
     # Test for 1.npz
     def test_1_npz(self):
         angle_pub = Angle_Publisher("1.npz", False, 4)
+        print("Testing 1.npz")
         angular_turn , end_of_row= angle_pub.anglePublishOnce()
+        print("The robot should turn", angular_turn, "degrees and end_of_row flag is", end_of_row)
         self.assertIsInstance(angular_turn, (int, float))
         self.assertEqual(end_of_row, False)
         self.assertTrue(-90.0 < angular_turn < 90.0)
@@ -18,7 +20,9 @@ class TestMyMath(unittest.TestCase):
     # I'm assuming even if the end_of_row flag is raised, the bot should not have angular velocity
     def test_2_npz(self):
         angle_pub = Angle_Publisher("2.npz", False, 4)
+        print("Testing 2.npz")
         angular_turn , end_of_row= angle_pub.anglePublishOnce()
+        print("The robot should turn", angular_turn, "degrees and end_of_row flag is", end_of_row)
         self.assertIsInstance(angular_turn, (int, float))
         self.assertEqual(end_of_row, True)
         self.assertTrue(-90.0 < angular_turn < 90.0)
@@ -26,7 +30,9 @@ class TestMyMath(unittest.TestCase):
     # Test for 3.npz
     def test_3_npz(self):
         angle_pub = Angle_Publisher("3.npz", False, 4)
+        print("Testing 3.npz")
         angular_turn , end_of_row= angle_pub.anglePublishOnce()
+        print("The robot should turn", angular_turn, "degrees and end_of_row flag is", end_of_row)
         self.assertIsInstance(angular_turn, (int, float))
         self.assertEqual(end_of_row, False)
         self.assertTrue(-90.0 < angular_turn < 90.0)
@@ -34,7 +40,9 @@ class TestMyMath(unittest.TestCase):
     # Test for 4.npz
     def test_4_npz(self):
         angle_pub = Angle_Publisher("4.npz", False, 4)
+        print("Testing 4.npz")
         angular_turn , end_of_row= angle_pub.anglePublishOnce()
+        print("The robot should turn", angular_turn, "degrees and end_of_row flag is", end_of_row)
         self.assertIsInstance(angular_turn, (int, float))
         self.assertEqual(end_of_row, False)
         self.assertTrue(-90.0 < angular_turn < 90.0)
